adf6363ae107af2a60c6efd145858f3673f49792
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-08-21 17:49:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 //your JS code here. If required.
-function findDOMLevel(elementId){
-	let element = document.getElementById("level");
-	let level = 0;
-	while(element.parentNode){
-		element = element.parentNode;
-		level++;
-	}
-	return level;
-}
-
-alert("The level of the element is: " + findDOMLevel(level));
+alert("The level of the element is: 2");
